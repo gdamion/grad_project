762029863e5dc94d5a93a06a1c8d2972722a9431
Alexander B <addlesssugar@gmail.com>
2020-04-16 22:42:14 +0300

    upd

---
+++ openPOWERLINK_V2/apps/demo_cn_console_my/build/linux/CMakeFiles/demo_cn_console.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo_cn_console.dir/src/main.c.o"
+  "CMakeFiles/demo_cn_console.dir/src/app.c.o"
+  "CMakeFiles/demo_cn_console.dir/src/event.c.o"
+  "CMakeFiles/demo_cn_console.dir/src/memory_api.c.o"
+  "CMakeFiles/demo_cn_console.dir/home/al/Code/diploma/EPL_ROS/openPOWERLINK_V2/apps/common/src/obdcreate/obdcreate.c.o"
+  "CMakeFiles/demo_cn_console.dir/home/al/Code/diploma/EPL_ROS/openPOWERLINK_V2/apps/common/src/eventlog/eventlog.c.o"
+  "CMakeFiles/demo_cn_console.dir/home/al/Code/diploma/EPL_ROS/openPOWERLINK_V2/apps/common/src/eventlog/eventlogstring.c.o"
+  "CMakeFiles/demo_cn_console.dir/home/al/Code/diploma/EPL_ROS/openPOWERLINK_V2/apps/common/src/netselect/netselect.c.o"
+  "CMakeFiles/demo_cn_console.dir/home/al/Code/diploma/EPL_ROS/openPOWERLINK_V2/contrib/console/printlog.c.o"
+  "CMakeFiles/demo_cn_console.dir/home/al/Code/diploma/EPL_ROS/openPOWERLINK_V2/apps/common/src/system/system-linux.c.o"
+  "CMakeFiles/demo_cn_console.dir/home/al/Code/diploma/EPL_ROS/openPOWERLINK_V2/contrib/console/console-linux.c.o"
+  "CMakeFiles/demo_cn_console.dir/home/al/Code/diploma/EPL_ROS/openPOWERLINK_V2/contrib/trace/trace-printf.c.o"
+  "demo_cn_console.pdb"
+  "demo_cn_console"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/demo_cn_console.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
